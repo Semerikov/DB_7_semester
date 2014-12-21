665e4b06b630be3a264e65dc95a1a2f83928989d
Sinitsin <ballack.tyta@gmail.com>
2014-12-21 16:59:19 +0300

    Add missed files

---
+++ BookStorageProject/BookStorage/BookStorage/Areas/USER/Models/OrderDetailsView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookStorage.Models.DAL;
+
+namespace BookStorage.Areas.USER.Models
+{
+    public class OrderDetailsView
+    {
+        public Order Order { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
